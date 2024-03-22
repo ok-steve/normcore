331167e93444e867e04a06944a8294c87369ecb6
Steve Cherry <2136364+ok-steve@users.noreply.github.com>
2024-03-22 09:27:53 -0400

    Move minification last

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,10 @@ module.exports = (ctx) => ({
   map: ctx.options.map,
   plugins: {
     "postcss-import": { root: ctx.file.dirname },
-    "postcss-minify": {},
     "postcss-preset-env": {
       minimumVendorImplementations: 2,
       preserve: true,
     },
+    "postcss-minify": {},
   },
 });
