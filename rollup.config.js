6195e23879e61e4fe33effa5ec62ee9029ed3a8b
Steve Cherry <2136364+ok-steve@users.noreply.github.com>
2025-03-03 02:24:59 +0000

    Add back javascript

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+const terser = require("@rollup/plugin-terser");
+
+module.exports = {
+  input: "src/js/normcore.js",
+  plugins: [terser()],
+  output: {
+    format: "iife",
+    file: "dist/js/normcore.js",
+    sourcemap: true,
+  },
+};
